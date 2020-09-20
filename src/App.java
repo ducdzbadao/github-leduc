ceba37ab6a2a496582b22f576fd6401b5dbb7401
ducdzbadao <71587323+ducdzbadao@users.noreply.github.com>
2020-09-20 17:37:55 +0700

    Add

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //String msg = "Fuck THis World";
+        System.out.printf("type: %-10s Size: %-2s Min: %-20s Max: %-20s",
+        Integer.TYPE, Integer.BYTES, Integer.MAX_VALUE, Integer.MAX_VALUE);
+    }
+}
